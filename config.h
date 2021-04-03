98683811bb1f5c02ed19eb044621bc4108fb4c7b
senpai-10 <bmjfdrh@gmail.com>
2021-04-03 11:41:21 +0300

    border

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 #define Terminal "kitty"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
