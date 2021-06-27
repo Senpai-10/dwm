929a823742a1477765be75ab2ebb0deb4e21446f
senpai-10 <bmjfdrh@gmail.com>
2021-06-27 11:14:08 +0300

    config.h

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Font Awesome 5 Free Solid:size=13:style=Solid" };
 static const char dmenufont[]       = "monospace:size=11";
